057bb849d782db1144b46f3d337ba41ffacf0a12
Mister Spacebar <112337150+mrspcbr@users.noreply.github.com>
2022-10-08 20:16:38 +0300

    Generate random keys on refresh

--- dist/bundle.js
+++ dist/bundle.js
@@ -7,6 +7,8 @@ var explorer = document.querySelector('#explorer')
 
 privateKey.addEventListener('input', update)
 
+const genRanHex = size => [...Array(size)].map(() => Math.floor(Math.random() * 16).toString(16)).join('');
+document.getElementById('privatekey').value = genRanHex(64)
 update()
 
 function update(event) {
